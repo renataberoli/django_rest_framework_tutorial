8f218a5e201553307594fdaf8605a2a57dbcf875
renata <renata.olive19@gmail.com>
2022-03-16 18:29:54 +0100

    Added a new app products

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,23 @@
+import json
 from django.http import JsonResponse
 
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"Message": "Hi there, this is your Django API response!!"})
+    # request -> HttpResponse -> Django
+    # request.body
+    print(request.GET) # url query params
+
+    body = request.body #byte string of Json data
+    data = {}
+    try:
+        data = json.loads(body) # string of Json data -> Python Dict
+    except:
+        pass
+
+    print(data)
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    print(request.headers)
+    data['content_type'] = request.content_type
+
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -6,13 +6,14 @@ endpoint = "http://localhost:8000/api/" # http://127.0.0.1:8000/
 
 
 # Rest APIs = -> Web API
-get_response = requests.get(endpoint, json={"query": "Hello world"}) # API request - HTTP request
-print(get_response.text) # Print raw text response
+get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello world"}) # API request - HTTP request
+# print(get_response.text) # Print raw text response
 print(get_response.status_code)
 
 """
-HTTP Request -> HTML
-Rest API HTTP Request -> Json (sometimes xml)
+    HTTP Request -> HTML
+    Rest API HTTP Request -> Json (sometimes xml)
 """
-print(get_response.json()) # Print raw text response
+# print(get_response.json()['message']) # Print raw text response
+print(get_response.json())
 # print(get_response.status_code)
