5352b4a610cd59e24f621655137077189b657479
renata <renata.olive19@gmail.com>
2022-03-16 15:34:26 +0100

    adding api project

---
+++ backend/api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+# from .views import api_home
+
+urlpatterns = [
+    path('', views.api_home) # localhost:8000/api/
+]
--- py_client/basic.py
+++ py_client/basic.py
@@ -1,16 +1,18 @@
 import requests
 
 # endpoint = "https://httpbin.org/status/200/"
-endpoint = "https://httpbin.org/anything"
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000/api/" # http://127.0.0.1:8000/
 
 
 # Rest APIs = -> Web API
 get_response = requests.get(endpoint, json={"query": "Hello world"}) # API request - HTTP request
 print(get_response.text) # Print raw text response
+print(get_response.status_code)
 
 """
 HTTP Request -> HTML
 Rest API HTTP Request -> Json (sometimes xml)
 """
 print(get_response.json()) # Print raw text response
-print(get_response.status_code)
+# print(get_response.status_code)
